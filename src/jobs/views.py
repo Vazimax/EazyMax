3b45e0e37e835516dfe17fcd9e3e3e5f80732cd5
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-10 12:10:05 +0100

    allowed host

--- src/jobs/views.py
+++ src/jobs/views.py
@@ -108,7 +108,7 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
 
 class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Job
-    success_url = '/'
+    #success_url = '/'
 
     def test_func(self):
         post = self.get_object()
